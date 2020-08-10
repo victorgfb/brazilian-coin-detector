f4d80dac19516e8a530f3493d16b95c212b6ec64
Victor Gabriel <victorgfb80@gmail.com>
2020-08-10 13:38:58 -0300

    melhora a separação das moedas

--- detectCoin.py
+++ detectCoin.py
@@ -23,14 +23,20 @@ avg_color_per_row = np.average(gray, axis=0)
 avg_color = np.average(avg_color_per_row, axis=0)
 print(avg_color)
 
-if(avg_color >= 125):
+if(avg_color >= 100):
     gray = cv2.bitwise_not(gray)
 
-res = cv2.bitwise_and(img,img,mask = gray)
+g = gray.copy()
+
+res = cv2.bitwise_and(img, img,mask = gray)
 gray = cv2.cvtColor( res, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (9, 9), 0)
+
+g2 = gray.copy()
 
 _,thresh = cv2.threshold(gray ,1,255,cv2.THRESH_BINARY)
 
+# thresh = cv2.GaussianBlur(thresh, (9, 9),0)
 
 thr = thresh.copy()
 
@@ -53,9 +59,11 @@ for label in np.unique(labels)[1:]:
     y = int(y)
     r = int(r)
 
-    r+=5
+    r+=10
 
     crop_img = save_img[(y -r):(y+ r), (x -r):(x+r)]
+    
+    #adicionar limite caso a moeda esteja no canto.
 
     cv2.imwrite(str(label) + ".jpg", crop_img)
 
@@ -63,8 +71,9 @@ for label in np.unique(labels)[1:]:
     cv2.putText(img, "#{}".format(label), (x - 10, y),cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
 
-cv2.imshow("teste", img)
-cv2.imshow("thr", crop_img)
-cv2.imshow("AND",res)
+cv2.imshow("teste", thr)
+cv2.imshow("thr", g2)
+cv2.imshow("AND", g)
+cv2.imshow("img", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
